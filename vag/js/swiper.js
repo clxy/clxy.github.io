c5469b8224dd21b74202c1ac97cb30ad928746d2
clxy <j_clxy@hotmail.com>
2023-11-21 12:57:38 +0800

    改善

--- vag/js/swiper.js
+++ vag/js/swiper.js
@@ -1,13 +1,22 @@
 
-const swiper = new Swiper(".intro-swiper", {
+const introSwiper = new Swiper(".intro-swiper", {
   grabCursor: true,
   centeredSlides: true,
   slidesPerView: "auto",
+  spaceBetween: 20,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+    dynamicBullets: true,
+    dynamicMainBullets: 2
   },
-
+  loop: true,
+  loopAdditionalSlides: 1,
+  speed: 1000,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
   effect: "coverflow",
   coverflowEffect: {
     rotate: 50,
@@ -26,3 +35,18 @@ const swiper = new Swiper(".intro-swiper", {
   // },
 
 })
+
+var newsSwiper = new Swiper(".news-swiper", {
+  slidesPerView: 2,
+  centeredSlides: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    // type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
