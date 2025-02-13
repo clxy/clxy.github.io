11ca6afe787793950fef5a6b40092d5260d716bc
clxy <j_clxy@hotmail.com>
2025-02-13 09:00:46 +0800

    一点是一点

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+console.log('App.vue')
 </script>
 
 <template>
   <UApp>
+
+    <header>
+      <h1>My App Header</h1>
+      <ULink to="/app">Go to App</ULink>
+      <ULink to="/home">Go to Home</ULink>
+      <ULink to="/creation">Go to Creation</ULink>
+    </header>
+
     <RouterView />
   </UApp>
 </template>
