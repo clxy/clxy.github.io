8af61b135ec2fcc1d807bb3e0445f839f636bb17
clxy <j_clxy@hotmail.com>
2025-03-16 22:13:40 +0800

    不知道啥来着

--- src/components/header.vue
+++ src/components/header.vue
@@ -3,7 +3,10 @@ import { ref, computed } from 'vue'
 import type { NavigationMenuItem } from '@nuxt/ui'
 import routes from '~pages'
 
+import Logo from './logo.vue'
+
 // 从routes meta里提取Menu信息
+// TODO: route.path是分层的 比如 /app, /app/stock
 const routesMeta: NavigationMenuItem[] = routes.map((route) => ({
   label: route?.meta?.title as string | undefined,
   icon: route?.meta?.icon as string | undefined,
@@ -11,61 +14,55 @@ const routesMeta: NavigationMenuItem[] = routes.map((route) => ({
 }))
 
 const items = ref([
-  [
-    { slot: 'logo' }
-  ],
+  [{ slot: 'header' }],
   [...routesMeta],
-  [
-    // {
-    //   label: 'GitHub',
-    //   icon: 'i-mdi-github',
-    //   to: 'https://github.com/clxy/clxy.github.io',
-    //   target: '_blank',
-    //   external: true
-    // },
-    // 上面的配置不好使。使用下面的按钮代替。ULink的Bug的可能性很大！！！
-    { slot: 'tail' },
-  ]
+  [{ slot: 'tail' },]
 ])
 
 const showBookmarks = ref(true)
 const showHistory = ref(false)
 const showDownloads = ref(false)
 
-const modeItems = computed(() => [{
-  label: 'Interface',
-  icon: 'i-lucide-app-window',
-  type: 'label' as const
-}, {
-  type: 'separator' as const
-}, {
-  label: 'Show Bookmarks',
-  icon: 'i-lucide-bookmark',
-  type: 'checkbox' as const,
-  checked: showBookmarks.value,
-  onUpdateChecked(checked: boolean) {
-    showBookmarks.value = checked
+const modeItems = computed(() => [
+  {
+    label: 'Interface',
+    icon: 'i-lucide-app-window',
+    type: 'label' as const
   },
-  onSelect(e: Event) {
-    e.preventDefault()
+  { type: 'separator' as const },
+  {
+    label: 'Show Bookmarks',
+    icon: 'i-lucide-bookmark',
+    type: 'checkbox' as const,
+    checked: showBookmarks.value,
+    onUpdateChecked(checked: boolean) {
+      showBookmarks.value = checked
+    },
+    onSelect(e: Event) {
+      e.preventDefault()
+    }
+  },
+  {
+    label: 'Show History',
+    icon: 'i-lucide-clock',
+    type: 'checkbox' as const,
+    checked: showHistory.value,
+    onUpdateChecked(checked: boolean) {
+      showHistory.value = checked
+    }
+  },
+  {
+    label: 'Show Downloads',
+    icon: 'i-lucide-download',
+    type: 'checkbox' as const,
+    checked: showDownloads.value,
+    onUpdateChecked(checked: boolean) {
+      showDownloads.value = checked
+    }
   }
-}, {
-  label: 'Show History',
-  icon: 'i-lucide-clock',
-  type: 'checkbox' as const,
-  checked: showHistory.value,
-  onUpdateChecked(checked: boolean) {
-    showHistory.value = checked
-  }
-}, {
-  label: 'Show Downloads',
-  icon: 'i-lucide-download',
-  type: 'checkbox' as const,
-  checked: showDownloads.value,
-  onUpdateChecked(checked: boolean) {
-    showDownloads.value = checked
-  }
-}])
+])
+
+const slideoverState = ref(false)
 </script>
 
 <template>
@@ -73,12 +70,25 @@ const modeItems = computed(() => [{
     <UNavigationMenu
       orientation="horizontal"
       :items="items"
-      class="max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between gap-3 h-(--ui-header-height)">
+      class="flex items-center justify-between gap-3 h-(--ui-header-height)">
+      <!-- class="max-w-(--ui-container) mx-auto px-4 sm:px-6 lg:px-8" -->
 
-      <template #logo>
-        <ULink to="/home">
-          <img src="/assets/logo.png" alt="Nuxt UI" class="h-6 w-auto" />
-        </ULink>
+      <template #header>
+
+        <USlideover side="left" v-model:open="slideoverState">
+          <UButton label="" color="neutral" variant="outline" icon="i-mdi-menu" />
+          <template #header>
+            <div class="flex items-center justify-between w-full h-(--ui-header-height)">
+              <Logo :noLink="true" />
+              <UButton @click="slideoverState = false" icon="i-mdi-arrow-left" variant="ghost" color="neutral" />
+            </div>
+          </template>
+          <template #body>
+            <div class="h-full" />
+          </template>
+        </USlideover>
+
+        <Logo />
       </template>
 
       <template #tail>
---
+++ src/components/logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <ULink to="/home" v-if="!noLink">
+    <LogoImage />
+  </ULink>
+  <LogoImage v-else />
+</template>
+
+<script setup lang="ts">
+
+const LogoImage = {
+  template: `<img src="/assets/logo.png" alt="Clxy Studio" class="h-6 w-auto" />`
+}
+
+defineProps({
+  // 控制是否禁用链接
+  noLink: {
+    type: Boolean,
+    default: false
+  },
+})
+</script>
+
+<style scoped></style>
